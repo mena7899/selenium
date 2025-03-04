54fb12f7456aa2868cf6c8bcae2b6283fbdd8f83
Mena Ibrahim Shehata Elias <melias@ISFPDOMAIN.com>
2025-03-04 10:15:13 +0200

    added fluent wait to wait utility class

--- src/main/java/com/rivtrans/utilities/WaitUtility.java
+++ src/main/java/com/rivtrans/utilities/WaitUtility.java
@@ -2,14 +2,18 @@ package com.rivtrans.utilities;
 
 import java.time.Duration;
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.StaleElementReferenceException;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class WaitUtility extends Utility {
 	
 	// explicit wait means if the ExpectedConditions didn't happened during a duration of seconds throw an error
 	//Waits for a specific condition before proceeding
-	public static void explicitWait(int seconds, By locator) {
+	public static void explicitWaitUntilVisible(int seconds, By locator) {
 		
 		WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(seconds));
 		// the driver in WebDriverWait is the diver from Utility (drive=BasePage.driver)
@@ -24,4 +28,14 @@ public class WaitUtility extends Utility {
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(seconds));
 	}
 	
+	
+	  public static void fluentWaitUntilVisible(int seconds, By locator,int milliseconds) {
+		   
+			FluentWait<WebDriver> fluentWait = new FluentWait<WebDriver>(driver)
+		            .withTimeout(Duration.ofSeconds(seconds))
+		            .pollingEvery(Duration.ofMillis(milliseconds))
+		            .ignoring(NoSuchElementException.class,
+		                    StaleElementReferenceException.class);
+		    fluentWait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+		  }
 }

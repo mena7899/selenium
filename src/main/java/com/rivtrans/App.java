fb3c999095e4e7c25990a5108d680f05ef4a1d7e
Mena Ibrahim Shehata Elias <melias@ISFPDOMAIN.com>
2025-03-22 15:35:53 +0200

    added buttons and methods to city page

--- src/main/java/com/rivtrans/App.java
+++ src/main/java/com/rivtrans/App.java
@@ -50,22 +50,28 @@ public class App
     	driver.findElement(By.cssSelector("a[class*='ml-sub-menu']")).click();
     	Thread.sleep(3000);
     	//click cites
-    	driver.findElement(By.cssSelector("a[href='/cities']")).click();
-    	//driver.findElement(By.cssSelector("a[href='/countries']")).click();
+    	//driver.findElement(By.cssSelector("a[href='/cities']")).click();
+    	driver.findElement(By.cssSelector("a[href='/countries']")).click();
     	Thread.sleep(3000);
-    	//click add button
-    	driver.findElement(By.cssSelector("mat-icon[class='mat-icon notranslate material-icons mat-ligature-font mat-icon-no-color']")).click();
+    	//click add city button
+    	//driver.findElement(By.cssSelector("mat-icon[class='mat-icon notranslate material-icons mat-ligature-font mat-icon-no-color']")).click();
     	Thread.sleep(3000);
-    	driver.findElement(By.cssSelector("div[class='mat-mdc-form-field-infix ng-tns-c508571215-12']")).click();
+    	//click countries dropdown list
+    	//driver.findElement(By.cssSelector("div[class='mat-mdc-form-field-infix ng-tns-c508571215-12']")).click();
     	Thread.sleep(3000);
-    	driver.findElement(By.xpath("//span[contains(text(),'مصر') or contains(text(),'Egypt') or contains(text(),'egypt')]")).click();
+    	//driver.findElement(By.xpath("//span[contains(text(),'مصر') or contains(text(),'Egypt') or contains(text(),'egypt')]")).click();
     	// //*[@id="mat-select-54-panel"]//*[@role="option"]  all the options in this list
     	// //*[@id='mat-select-54-panel']//*[@role='option']//span[contains(text(),'مصر')]  //find the option by inner text
     	//List<WebElement> list =driver.findElements(By.cssSelector("span[class='mdc-list-item__primary-text']"));
-    	//List<WebElement> list =driver.findElements(By.cssSelector("mat-icon[class=\'mat-icon notranslate material-icons mat-ligature-font delete-icon mat-icon-no-color\']"));
+    	Thread.sleep(3000);
+    	//delete buttons
+    	List<WebElement> list =driver.findElements(By.cssSelector("mat-icon[class=\'mat-icon notranslate material-icons mat-ligature-font delete-icon mat-icon-no-color\']"));
     	//List<WebElement> list =driver.findElements(By.cssSelector("mat-option[role='option']"));
     	//System.out.print(list);
-    	//list.get(0).click();
+    	list.get(0).click();
+    	Thread.sleep(3000);
+    	 driver.findElement(By.xpath("//span[@class='mdc-button__label' and contains(text(), 'حذف')]")).click();
+    	//driver.findElement(By.cssSelector("div[class='mat-mdc-form-field-infix ng-tns-c508571215-12']")).click();
     	//System.out.print(list);
     	//Select select =new Select(driver.findElement(By.cssSelector("div[class='mat-mdc-form-field-infix ng-tns-c508571215-12']")));
     	//select.selectByIndex(1);
--- src/main/java/com/rivtrans/pages/CitiesPage.java
+++ src/main/java/com/rivtrans/pages/CitiesPage.java
@@ -10,7 +10,61 @@ public class CitiesPage extends BasePage {
 	private By add_city_window_arabic_name_input = By.cssSelector("mat-icon[class='mat-icon notranslate material-icons mat-ligature-font mat-icon-no-color']");
 	private By add_city_window_english_name_input = By.cssSelector("mat-icon[class='mat-icon notranslate material-icons mat-ligature-font mat-icon-no-color']");
 	private By add_city_window_countries_dropdown_list = By.cssSelector("mat-icon[class='mat-icon notranslate material-icons mat-ligature-font mat-icon-no-color']");
-	private By add_city_window_iso_code= By.cssSelector("mat-icon[class='mat-icon notranslate material-icons mat-ligature-font mat-icon-no-color']");
+	private By add_city_window_iso_code_input= By.cssSelector("mat-icon[class='mat-icon notranslate material-icons mat-ligature-font mat-icon-no-color']");
+	private By add_city_window_add_button = By.cssSelector("button[class=\'mdc-button mdc-button--raised mat-mdc-raised-button mat-primary mat-mdc-button-disabled mat-mdc-button-base\']");
 	private By add_city_success_message = By.cssSelector("div[class=\'snackbar success ng-star-inserted\']");
+	
+	public void clickAddCityButton() {
+		click(add_city_button);
+	}
+	
+	public void addCityWindowSetArabicName(String arabic_name) {
+		set(add_city_window_arabic_name_input, arabic_name);
+	}
+	
+	public void addCityWindowSetEnglishName(String english_name) {
+		set(add_city_window_english_name_input, english_name);
+	}
+	
+	public void addCityWindowSelectCountry(String arabic_name,String english_name) {
+		click(add_city_window_countries_dropdown_list);
+		String countryEnglish = english_name;
+		String countryArabic = arabic_name;
+		String xpathExpression = String.format("//span[contains(translate(text(), 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',"
+				+ " 'abcdefghijklmnopqrstuvwxyz'), '%s') or contains(text(), '%s')]", countryEnglish, countryArabic);
+		click(By.xpath(xpathExpression));
+		
+	}
+	
+	public void addCityWindowSelectCountryArabic(String arabic_name) {
+		click(add_city_window_countries_dropdown_list);
+		String countryArabic = arabic_name;
+		String xpathExpression = String.format("//span[contains(text(), '%s')]", countryArabic);
+		click(By.xpath(xpathExpression));
+		
+	}
+	
+	public void addCityWindowSelectCountryEnglish(String english_name) {
+		click(add_city_window_countries_dropdown_list);
+		String countryEnglish = english_name;
+		String xpathExpression = String.format("//span[contains(translate(text(), 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',"
+				+ " 'abcdefghijklmnopqrstuvwxyz'), '%s')]", countryEnglish);
+		click(By.xpath(xpathExpression));
+		
+	}
+	
+	public void addCityWindowSetIsoCode(String iso_code) {
+		set(add_city_window_iso_code_input, iso_code);
+	}
+	
+	public void addCityWindowClickAddButton() {
+		
+		click(add_city_window_add_button);
+	}
+	
+	public boolean AddCitySuccessMessageIsDisplayed() {		
+		
+		return find(add_city_success_message).isDisplayed();
+	}
 
 }
